3a110e51a291c38dc7c21167c307777d3258c84e
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-07-19 11:31:03 -0600

    updated

--- final-project-hatch/final/final_role-creation.sql
+++ final-project-hatch/final/final_role-creation.sql
@@ -1,28 +1,8 @@
-USE [HR_db]
-GO
-CREATE ROLE [final_proj_testers]
-GO
-USE [HR_db]
-GO
-ALTER ROLE [final_proj_testers] ADD MEMBER [final_running_buddy]
-GO
-USE [HR_db]
-GO
-ALTER ROLE [final_proj_testers] ADD MEMBER [final_test_user]
-GO
-use [HR_db]
-GO
-GRANT SELECT ON [hr].[employees] TO [final_proj_testers]
-GO
-use [HR_db]
-GO
-GRANT SELECT ON [hr].[dependents] TO [final_proj_testers]
-GO
-use [HR_db]
-GO
-GRANT SELECT ON [dbo].[final_view] TO [final_proj_testers]
-GO
 use [HR_db]
 GO
 GRANT SELECT ON [office].[locations] TO [final_proj_testers]
+GO
+use [HR_db]
+GO
+GRANT SELECT ON SCHEMA::[hr] TO [final_proj_testers]
 GO
