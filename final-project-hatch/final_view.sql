1bcfac0693ef4dcc97c415a39f0f77ff2bf9b677
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-07-19 10:03:49 -0600

    final project view creation

---
+++ final-project-hatch/final_view.sql
@@ -0,0 +1,25 @@
+USE HR_db;
+GO
+
+CREATE VIEW final_view AS
+
+SELECT 
+	CONCAT(emp.first_name, ' ', emp.last_name) AS 'Employee Name'
+	, CONCAT(depn.first_name, ' ', depn.last_name) AS 'Employee Dependent'
+	, depn.relationship AS 'Relationship'
+	--, j.job_title AS 'Job'
+FROM
+	office.departments d
+INNER JOIN
+	hr.employees emp
+ON
+	d.department_id = emp.department_id
+INNER JOIN
+	hr.dependents depn 
+ON 
+	emp.employee_id = depn.employee_id
+WHERE 
+	emp.last_name LIKE '%r'
+OR
+	emp.last_name LIKE '%a';
+GO
